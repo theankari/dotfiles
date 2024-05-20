3df54f2d4c0f30f58d2b178b86970101be460751
Aron Szabo <aron.szabo@cashlink.de>
2024-05-20 18:45:33 +0300

    Make polybar script dynamic

--- .config/polybar/polylauncher.sh
+++ .config/polybar/polylauncher.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,14 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-TRAY_POS=right MONITOR=eDP polybar example &
-MONITOR=HDMI1 polybar example &
-MONITOR=DisplayPort-0 polybar example &
-MONITOR=LVDS-1 polybar example &
-MONITOR=DP-1 polybar example &
-MONITOR=DP-2 polybar example &
-MONITOR=DP-3 polybar example &
+
+# Get monitors
+MONITORS=($(xrandr --listactivemonitors |  tail -n +2 | grep -oE '[^ ]+$'))
+MONITORLENGTH=${#MONITORS[@]}
 
 
-echo "Bars launched..."
+for (( i=0; i<${MONITORLENGTH}; i++ ));
+do
+  if [ "$i" -eq 0 ]; then
+    echo "launching tray on ${MONITORS[$i]}"
+    TRAY_POS=right MONITOR=${MONITORS[$i]} polybar example &
+  else
+    MONITOR=${MONITORS[$i]} polybar example &
+  fi
+done
+
